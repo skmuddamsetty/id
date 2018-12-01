93b61f79ee13cf14b52b99ce5c16c6d9f3d15140
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2018-12-01 08:16:53 -0500

    Added Add role button and validation funtionality

--- src/app/role-list/role-list.page.ts
+++ src/app/role-list/role-list.page.ts
@@ -1,24 +1,32 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Subscription } from 'rxjs';
-import { ModalController } from '@ionic/angular';
+import {
+  ModalController,
+  AlertController,
+  ToastController
+} from '@ionic/angular';
 import { DataService } from '../services/data.service';
 import { AngularFirestore } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-role-list',
   templateUrl: './role-list.page.html',
-  styleUrls: ['./role-list.page.scss'],
+  styleUrls: ['./role-list.page.scss']
 })
 export class RoleListPage implements OnInit, OnDestroy {
   public initForm = [];
   selectableRoles = [];
   selectedRole: any;
   selectedTechsKeys = [];
-  techMap = new Map();
+  roleMap = new Map();
   rolesSubscription: Subscription;
-  constructor(private modalCtrl: ModalController,
+  constructor(
+    private modalCtrl: ModalController,
     public dataService: DataService,
-    private readonly afs: AngularFirestore) { }
+    private readonly afs: AngularFirestore,
+    private alertController: AlertController,
+    private toastController: ToastController
+  ) {}
 
   ngOnInit() {
     this.rolesSubscription = this.afs
@@ -27,10 +35,13 @@ export class RoleListPage implements OnInit, OnDestroy {
       .subscribe((res: any) => {
         this.initForm = res[0].roles;
         this.selectableRoles = this.initForm.slice();
+        this.selectableRoles.forEach((role) => {
+          this.roleMap.set(role.value.toLowerCase(), role.value.toLowerCase());
+        });
       });
   }
 
-  onRoleSelect(role: any){
+  onRoleSelect(role: any) {
     this.selectedRole = role;
   }
 
@@ -42,7 +53,7 @@ export class RoleListPage implements OnInit, OnDestroy {
   onFilterRole(searchText: Event) {
     const text = searchText as any;
     if (text.detail.value) {
-      this.selectableRoles = this.selectableRoles.filter(role =>
+      this.selectableRoles = this.selectableRoles.filter((role) =>
         role.value.toLowerCase().includes(text.detail.value.toLowerCase())
       );
     } else {
@@ -50,10 +61,70 @@ export class RoleListPage implements OnInit, OnDestroy {
     }
   }
 
-  ngOnDestroy(){
-    if(this.rolesSubscription){
+  onAddNewRole() {
+    this.presentAlertPrompt();
+  }
+
+  async presentAlertPrompt() {
+    const alert = await this.alertController.create({
+      header: 'Add New Role!',
+      inputs: [
+        {
+          name: 'newRole',
+          type: 'text',
+          placeholder: 'Enter Role'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: () => {
+            console.log('Confirm Cancel');
+          }
+        },
+        {
+          text: 'Submit',
+          handler: (data) => {
+            if (data.newRole) {
+              if (!this.roleMap.get(data.newRole.toLowerCase())) {
+                this.selectableRoles.push({
+                  value: data.newRole,
+                  key: data.newRole
+                });
+                this.afs
+                  .doc<any>('roles/' + 'to9MEf8HET4JIANC4Cgr')
+                  .update({
+                    roles: this.selectableRoles
+                  })
+                  .then(() => {
+                    this.presentToast('New Role has been added Successfully!');
+                  })
+                  .catch((err) => {
+                    this.presentToast('Sorry, Try again!');
+                  });
+              }
+            }
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
+
+  async presentToast(message: string) {
+    const toast = await this.toastController.create({
+      message: message,
+      duration: 2000
+    });
+    toast.present();
+  }
+
+  ngOnDestroy() {
+    if (this.rolesSubscription) {
       this.rolesSubscription.unsubscribe();
     }
   }
-
 }
